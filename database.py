4986073ec42832c9e10a5ec74d4eeb25f04b6f97
Abhijeet <abhijeet.bhatiya@gmail.com>
2026-07-07 23:45:05 +0530

    initial commit

---
+++ database.py
@@ -0,0 +1,6 @@
+"""
+All the database work lives here (Lesson 12).
+
+Keeping the database code in its own file makes app.py shorter and easier to
+read. app.py just imports these functions and uses them.
+"""
